e2aa09166a9e5787fad4a2c3dd49662ddc403e06
Luferov1 <ldima12022002@gmail.com>
2023-03-20 17:49:30 +0300

    feat: add starting settings

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+const App = () => (
+  <div className={styles.header}>
+    Hello world
+  </div>
+);
+
+export default App;
