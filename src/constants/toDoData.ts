89358a1779d0bf663cf43dc5179723fe077fda98
Luferov1 <ldima12022002@gmail.com>
2023-03-22 14:42:47 +0300

    feat: add to do data

---
+++ src/constants/toDoData.ts
@@ -0,0 +1,16 @@
+import { IToDOView } from '../interfaces';
+
+const toDoData: IToDOView[] = [
+  {
+    header: 'Do this job man',
+    text: 'Finally do it man. We need you',
+    tags: ['wow', 'essketit', 'man'],
+  },
+  {
+    header: 'Check weather',
+    text: 'You must check th weather',
+    tags: ['weather', 'interesting', 'fun'],
+  },
+];
+
+export default toDoData;
