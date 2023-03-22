217ee0f4731cea93b3a970dfe05816371932d4bf
Luferov1 <ldima12022002@gmail.com>
2023-03-22 14:44:55 +0300

    feat: add to do item edit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.scss';
+import Aside from './components/Aside';
+import Article from './components/Article';
+import ToDoData from './constants/toDoData';
+import { ITagState, IToDoData } from './interfaces';
+import createUniqueArrayOfTags from './functions';
 
-const App = () => (
-  <div className={styles.header}>
-    Hello world
-  </div>
-);
+const App = () => {
+  const tagsArr: ITagState[] = createUniqueArrayOfTags(ToDoData);
+  const toDoListInitialState: IToDoData[] = ToDoData.map((item) => ({
+    ...item,
+    isEditing: false,
+  }));
+
+  const [data, setData] = useState(toDoListInitialState);
+  const [tags, setTags] = useState(tagsArr);
+  return (
+    <main className={styles.container}>
+      <Aside state={tags} setState={setTags} />
+      <Article
+        data={data}
+        tagsData={tags}
+        setData={setData}
+        setTags={setTags}
+      />
+    </main>
+  );
+};
 
 export default App;
