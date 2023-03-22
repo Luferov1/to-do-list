0b794cdee800309978e3b3b51df6e618d4ed4473
Luferov1 <ldima12022002@gmail.com>
2023-03-22 14:45:45 +0300

    feat: add tag button

---
+++ src/components/ToDoItemEdit/TagButton/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ITagState } from '../../../interfaces';
+import styles from './style.module.scss';
+
+interface Props {
+  state: ITagState[],
+  setState: React.Dispatch<React.SetStateAction<ITagState[]>>
+  index: number
+}
+
+const TagButton = ({ state, setState, index }: Props) => (
+  <button
+    type="button"
+    className={state[index].isActive ? styles.tagButton_active : styles.tagButton_disabled}
+    onClick={() => {
+      const newState = [...state];
+      newState[index] = {
+        ...newState[index],
+        isActive: !newState[index].isActive,
+      };
+      setState(newState);
+    }}
+  >
+    {String(state[index].name)}
+  </button>
+);
+
+export default TagButton;
